d182914b1785f02d1907583493378fc056c603a8
Echarghaoui Aymen <aymen.echarghaoui@gmail.com>
2022-05-20 01:06:15 +0200

    grouping articles

--- articles_grouping.py
+++ articles_grouping.py
@@ -8,7 +8,9 @@ groups = list(df['product_group_name'].unique())
 print(groups)
 
 for group in groups:
-    os.mkdir('/home/aymen/data/'+group[1:-1])
-
+    #os.mkdir('/home/aymen/data/'+group[1:-1])
+    print(group[1:-1])
+'''
 for i,row in df.iterrows():
     os.replace('/home/aymen/data/images_all/'+str(row['article_id'])+'.jpg','/home/aymen/data/'+row['product_group_name']+'/'+row['article_id']+'.jpg')
+'''
