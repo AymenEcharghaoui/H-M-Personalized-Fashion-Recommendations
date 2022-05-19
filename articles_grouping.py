08da619e46f68130d16598fda4ce814fd53854f7
Echarghaoui Aymen <aymen.echarghaoui@gmail.com>
2022-05-20 01:24:55 +0200

    grouping articles

--- articles_grouping.py
+++ articles_grouping.py
@@ -2,12 +2,12 @@ import pandas as pd
 import os
 
 
-df = pd.read_csv('/home/aymen/data/articles.csv',dtype={'article_id':str})
+df = pd.read_csv('/home/aymen/data/articles.csv',dtype={'article_id':str,'product_group_name':str})
 
 groups = list(df['product_group_name'].unique())
-print(groups)
+print(type(groups[0]))
 
-
+'''
 for group in groups:
     if(group  != 'Underwear/nightwear'):
         os.mkdir('/home/aymen/data/'+group)
@@ -18,3 +18,4 @@ for i,row in df.iterrows():
     else:
         os.replace('/home/aymen/data/images_all/'+str(row['article_id'])+'.jpg','/home/aymen/data/Underwear/'+row['article_id']+'.jpg')
         os.replace('/home/aymen/data/images_all/'+str(row['article_id'])+'.jpg','/home/aymen/data/Nightwear/'+row['article_id']+'.jpg')
+'''
