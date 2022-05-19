9f0f0211688bff3075a4ae1ab3c65dc102c8d416
Echarghaoui Aymen <aymen.echarghaoui@gmail.com>
2022-05-20 01:16:55 +0200

    grouping articles

--- articles_grouping.py
+++ articles_grouping.py
@@ -7,11 +7,14 @@ df = pd.read_csv('/home/aymen/data/articles.csv',dtype={'article_id':str})
 groups = list(df['product_group_name'].unique())
 print(groups)
 
-os.mkdir('/home/aymen/data/Underwear')
+
 for group in groups:
     if(group  != 'Underwear/nightwear'):
         os.mkdir('/home/aymen/data/'+group)
 
 for i,row in df.iterrows():
-    prod_type = row['product_group_name'] if row['product_group_name']!= 'Underwear/nightwear' else 'Underwear'
-    os.replace('/home/aymen/data/images_all/'+str(row['article_id'])+'.jpg','/home/aymen/data/'+prod_type+'/'+row['article_id']+'.jpg')
+    if row['product_group_name']!= 'Underwear/nightwear':
+        os.replace('/home/aymen/data/images_all/'+str(row['article_id'])+'.jpg','/home/aymen/data/'+row['product_group_name']+'/'+row['article_id']+'.jpg')
+    else:
+        os.replace('/home/aymen/data/images_all/'+str(row['article_id'])+'.jpg','/home/aymen/data/Underwear/'+row['article_id']+'.jpg')
+        os.replace('/home/aymen/data/images_all/'+str(row['article_id'])+'.jpg','/home/aymen/data/Nightwear/'+row['article_id']+'.jpg')
