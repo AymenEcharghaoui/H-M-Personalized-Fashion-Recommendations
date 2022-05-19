9d707e73b9839adbba5f592a20f4d2b1b98c58ed
Echarghaoui Aymen <aymen.echarghaoui@gmail.com>
2022-05-20 01:10:54 +0200

    grouping articles

--- articles_grouping.py
+++ articles_grouping.py
@@ -7,10 +7,11 @@ df = pd.read_csv('/home/aymen/data/articles.csv',dtype={'article_id':str})
 groups = list(df['product_group_name'].unique())
 print(groups)
 
+os.mkdir('/home/aymen/data/Underwear')
 for group in groups:
-    #os.mkdir('/home/aymen/data/'+group[1:-1])
-    print(group[1:-1])
-'''
+    if(group  != 'Underwear/nightwear'):
+        os.mkdir('/home/aymen/data/'+group)
+
 for i,row in df.iterrows():
-    os.replace('/home/aymen/data/images_all/'+str(row['article_id'])+'.jpg','/home/aymen/data/'+row['product_group_name']+'/'+row['article_id']+'.jpg')
-'''
+    prod_type = row['product_group_name'] if row['product_group_name']!= 'Underwear/nightwear' else 'Underwear'
+    os.replace('/home/aymen/data/images_all/'+str(row['article_id'])+'.jpg','/home/aymen/data/'+prod_type+'/'+row['article_id']+'.jpg')
