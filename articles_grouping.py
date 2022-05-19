503cdc0e041fd54be03631777f0a17e32f86be5a
Echarghaoui Aymen <aymen.echarghaoui@gmail.com>
2022-05-20 00:53:53 +0200

    grouping articles

--- articles_grouping.py
+++ articles_grouping.py
@@ -6,9 +6,9 @@ df = pd.read_csv('/home/aymen/data/articles.csv',dtype={'article_id':str})
 
 groups = list(df['product_group_name'].unique())
 print(groups)
-'''
+
 for group in groups:
-    os.mkdir('/home/aymen/data/'+group)
-'''
+    os.mkdir('/home/aymen/data/'+group[1:-1])
+
 for i,row in df.iterrows():
     os.replace('/home/aymen/data/images_all/'+str(row['article_id'])+'.jpg','/home/aymen/data/'+row['product_group_name']+'/'+row['article_id']+'.jpg')
