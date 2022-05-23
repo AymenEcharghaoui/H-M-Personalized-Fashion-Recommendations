f55a3394edbbbf4fb4e19dad4da5184ec847fa02
SHI Biao <biao.shi@polytechnique.edu>
2022-05-23 11:54:51 +0200

    update train

--- articles_info.py
+++ articles_info.py
@@ -27,20 +27,20 @@ im.close()
 
 print('number of article in images__all is', len(im_set))
 
-ar_set = set(df['article_id'].unique())
-tr_set = set(tr_df['article_id'].unique())
+# ar_set = set(df['article_id'].unique())
+# tr_set = set(tr_df['article_id'].unique())
 
-print('articles in articles.csv not in transactions_train.csv:', len(ar_set - tr_set))
+# print('articles in articles.csv not in transactions_train.csv:', len(ar_set - tr_set))
 
-print('articles in transactions_train.csv not in articles.csv:', len(tr_set - ar_set))
+# print('articles in transactions_train.csv not in articles.csv:', len(tr_set - ar_set))
 
-print('articles in articles.csv not in images__all:', len(ar_set - im_set))
+# print('articles in articles.csv not in images__all:', len(ar_set - im_set))
 
-print('articles in images__all not in articles.csv:', len(im_set - ar_set))
+# print('articles in images__all not in articles.csv:', len(im_set - ar_set))
 
-print('articles in images__all not in transactions_train.csv:', len(im_set - tr_set))
+# print('articles in images__all not in transactions_train.csv:', len(im_set - tr_set))
 
-print('articles in transactions_train.csv not in aimages__all:', len(tr_set - im_set))
+# print('articles in transactions_train.csv not in aimages__all:', len(tr_set - im_set))
 
 
 
